3baca674429c455cafb01d7c1c55fb6e5c38ea49
kaluzsikc <kaluzsikc@gmail.com>
2018-08-17 14:22:59 +0100

    Create db tables

---
+++ db/cc_cinema.sql
@@ -0,0 +1,21 @@
+DROP TABLE tickets;
+DROP TABLE customers;
+DROP TABLE films;
+
+CREATE TABLE customers (
+  id SERIAL4 PRIMARY KEY,
+  name VARCHAR(255) NOT NULL,
+  funds INT4 NOT NULL
+);
+
+CREATE TABLE films (
+  id SERIAL4 PRIMARY KEY,
+  title VARCHAR(255) NOT NULL,
+  price INT4 NIT NULL
+);
+
+CREATE TABLE tickets (
+  id SERIAL4 PRIMARY KEY,
+  customer_id INT4 REFERENCES customer(id) ON DELETE CASCADE,
+  film_id INT4 REFERENCES film(id) ON DELETE CASCADE
+);
