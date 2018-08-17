6ce8ef98ab04a523eab8ee110c5d39cf457d6ea4
kaluzsikc <kaluzsikc@gmail.com>
2018-08-17 14:32:06 +0100

    Add sql_runner and save method for customers

---
+++ models/customer.rb
@@ -0,0 +1,23 @@
+require_relative("../db/sql_runner")
+
+class Customer
+
+  attr_reader :id
+  attr_accessor :name, :funds
+
+  def initialize(options)
+    @id = options['id'].to_i if options['id']
+    @name = options['name']
+    @funds = options['funds']
+  end
+
+  def save()
+    sql = "INSERT INTO customers
+      (name, funds)
+      VALUES
+      ($1, $2) RETURNING *"
+    values = [@name, @funds]
+    customers = SqlRunner.run(sql, values)
+    @id = customers['id'].to_i
+  end
+end
