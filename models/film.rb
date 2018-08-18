22993688cba1842beeabd29601819f91095d1ace
kaluzsikc <kaluzsikc@gmail.com>
2018-08-18 17:38:38 +0100

    Add update method on films

--- models/film.rb
+++ models/film.rb
@@ -29,5 +29,14 @@ class Film
     return result
   end
 
-
+  def update()
+    sql = "UPDATE films
+          SET
+          (title, price)
+          =
+          ($1, $2)
+          WHERE id = $3"
+    values = [@title, @price,@id]
+    SqlRunner.run( sql, values)
+  end
 end
